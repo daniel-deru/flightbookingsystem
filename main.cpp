07d1fedc006dcc88c4b925323ea576cac8d4a19f
daniel <danielderu@gmail.com>
2022-05-05 22:19:18 +0200

    added function to get seat

--- main.cpp
+++ main.cpp
@@ -5,9 +5,11 @@
 
 // Constants 
 const float ECO_PRICE = 1600.00;
+const int SEATS_PER_ROW = 6;
 const int TOTAL_SEATS = 50;
 const int FLIGHTS = 5;
 const int PAIRS = 2;
+
 const std::string flighttimes[FLIGHTS][PAIRS] = 
 {
     {"7:00", "9:30"},
@@ -27,30 +29,23 @@ struct Flights {
     int flightnum;
     std::string depart;
     std::string arrive;
-    Seating* seats[TOTAL_SEATS];
+    Seating seats[TOTAL_SEATS];
 };
 
 // Function declarations
 std::string getname();
-int getflight();
-std::string getseat();
-Seating* create_seats();
-// std::string to_string(int a);
+Flights getflight(Flights &flights);
+std::string getseat(Flights flight);
 Flights* createflights(const std::string flighttimes[FLIGHTS][PAIRS]);
-
-
 
 int main(){
     // Check if the user is ready to leave the main loop
     bool done = false;
-    Flights* flights = createflights(flighttimes);
-    std::cout << flights[0].seats[0]->seat << std::endl;
-
-    // while(!done){
-        // std::string name = getname();
-        // int flight = getflight();
-        
-    // }
+    // Contstruct the flights object that will be used as the main flight data structure
+    Flights *flights = createflights(flighttimes);
+    // std::string name = getname();
+    Flights flight = getflight(*flights);
+    std::string seat = getseat(flight);
 
     system("pause>0");
     return 0;
@@ -63,15 +58,16 @@ std::string getname(){
     return name;
 }
 
-int getflight(){
+Flights getflight(Flights &flights_pointer){
     int time;
+    Flights* flights = &flights_pointer;
     std::string message = "Choose the time by entering the option number from the displayed list:";
     bool valid_time = false;
 
     std::cout << "The available flight times are:\n\tDepart\tArrive" << std::endl;
 
     for(int i = 0; i < FLIGHTS; i++){
-        std::cout <<  (i + 1) << ".\t" << flighttimes[i][0] << "\t" << flighttimes[i][1] << std::endl;
+        std::cout <<  (i + 1) << ".\t" << flights[i].depart << "\t" << flights[i].arrive << std::endl;
     }
 
     while(!valid_time){
@@ -86,17 +82,24 @@ int getflight(){
         }
     }
 
-    return time;
+    return flights[time];
 }
 
-std::string getseat(){
-    std::cout << "" << std::endl;
+std::string getseat(Flights flight){
+    std::cout << "The available seats for " << flight.depart << " are as follows:" << std::endl;
+     std::cout << "|" << flight.seats[0].seat << "|" << std::endl;
+     std::cout << "|" << flight.seats[1].seat << "|" << std::endl;
+     std::cout << "|" << flight.seats[2].seat << "|" << std::endl;
+     std::cout << "|" << flight.seats[3].seat << "|" << std::endl;
+    // for(int row = 0; row <= TOTAL_SEATS; row++){
+    //      std::cout << "|" << flight.seats[row]->seat << "|" << std::endl;
+    // }
     return "";
 }
 
 Seating* create_seats(){
     static Seating seats[TOTAL_SEATS];
-    const int MAX_ROW_NUMS = 6;
+
     // ASCII value for A
     const int from = 65;
     // ASCII value for I
@@ -104,7 +107,7 @@ Seating* create_seats(){
     int k = 0;
     
     for(int i = 0; i <= (to - from); i++){
-        for(int j = 1; j <= MAX_ROW_NUMS; j++){
+        for(int j = 1; j <= SEATS_PER_ROW; j++){
             std::string num = char(i + from) + to_string(j);
             Seating seat = {num, false};
             seats[k] = seat;
@@ -121,14 +124,15 @@ Flights* createflights(const std::string flighttimes[FLIGHTS][PAIRS]){
     static Flights flights[FLIGHTS];
 
     for(int i = 0; i < FLIGHTS; i++){
-        Seating* seats = create_seats();
+        static Seating *seats = create_seats();
         int flightnum = i + 1;
         std::string depart = flighttimes[i][0];
         std::string arrive = flighttimes[i][1];
-        Flights flight = {flightnum, depart, arrive, seats};
+
+        Flights flight = {flightnum, depart, arrive, *seats};
+        // std::cout << flight.seats[1].seat << std::endl;
         flights[i] = flight;
     }
 
     return flights;
 }
-
